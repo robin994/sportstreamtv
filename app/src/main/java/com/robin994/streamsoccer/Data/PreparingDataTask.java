5276420b5cb6232567c306e8a1bfe71d8f05f3c7
Roberto <robin994@hotmail.it>
2018-04-29 17:32:27 +0200

    fix sort matches

--- app/src/main/java/com/robin994/streamsoccer/Data/PreparingDataTask.java
+++ app/src/main/java/com/robin994/streamsoccer/Data/PreparingDataTask.java
@@ -57,7 +57,7 @@ public class PreparingDataTask extends AsyncTask<Void, Void, ArrayObjectAdapter>
         Collections.sort(list, new Comparator<Entry>() {
             @Override
             public int compare(Entry o1, Entry o2) {
-                return o1.getDate().compareTo(o2.getDate());
+                return o2.getDate().compareTo(o1.getDate());
             }
         });
 
